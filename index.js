da3e03278e26a9664399908e2313201e6e922d0d
akinjed5 <62756856+akinjed5@users.noreply.github.com>
2021-01-01 23:51:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    var yearsRemaining = 90-age ;
+    //Write your code here.
+    var x = yearsRemaining * 365;
+    var y = yearsRemaining * 52;
+    var z = yearsRemaining * 12;
+    console.log("You have " + x + " days, " + y + " weeks, and " + z +" months left.")
+    
+    
+    
+    
+/*************Don't change the code below**********/
+}
+lifeInWeeks()
